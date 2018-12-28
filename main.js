dfae752e8bc90001b2fb60c1f1688798dbeb8b11
rmnbrtshk <rmnbrtshk@gmail.com>
2018-12-28 16:55:32 +0200

    fix formating

--- main.js
+++ main.js
@@ -1,67 +1,14 @@
 "use strict"
-function getName() {
-  return this.name;
-}
 var obj = {
   name: 'Ronny'
 }
 obj.getName = getName;
 console.log(obj.getName());
 // getName(); /*error with use strict. Better use getName.call(obj)*/
-
-function getDoubled() {
-  return this.number = this.number * 2;
-}
-
 var numObj = {
   number: 2
-}
-
-function getDoubledTrippled(){
-  return getDoubled.call(numObj) * 3;
-}
+};
 console.log(getDoubledTrippled());
-
-function Car(brand, year, run, color) {
-  this.brand = brand;
-  this.year = year;
-  this.run = run;
-  this.color = color;
-  this.engine = false;
-  this.traffic = false;
-  this.fuel = false;
-
-  this.startEngine = function() {
-    if(this.fuel === true) {
-      this.engine = true;
-    } else {
-      console.log('Нужно заправить автомобиль');
-    }
-  }
-
-  this.go = function() {
-    if(this.engine === true) {
-      console.log('Машина ' + this.brand + ' марки ' + this.color + ' цвета поехала!');
-      this.traffic = true;
-    } else {
-      console.log('Включите вначале зажигание!');
-    }
-  };
-
-  this.stop = function() {
-    if(this.engine === true) {
-      console.log('Машинка остановилась!');
-      this.engine = false;
-      this.traffic = false;
-    } else {
-      console.log('Зажигание и так выключено');
-    }
-  };
-
-  this.addFuel = function() {
-    this.fuel = true;
-  };
-}
 
 var objCar = new Car('bmw', '2019', '9', 'black');
 var objSecondCar = new Car('mercedesBenz', '2019', '99', 'silver');
@@ -80,24 +27,6 @@ objCar.addFuel();
 objCar.startEngine();
 objSecondCar.go();
 objSecondCar.stop();
-
-function City(name, population, country) {
-  this.name = name;
-  this.population = population;
-  this.country = country;
-
-  this.getPopulation = function() {
-    return this.population;
-  };
-
-  this.getCityName = function() {
-    return this.name;
-  };
-
-  this.addCitizen = function() {
-    return this.population++;
-  };
-}
 var city = new City('Sydney', 4840600, 'Australia');
 var city2 = new City('Kyiv', 2887974, 'Ukraine');
 var city3 = new City('Lviv', 727968, 'Ukraine');
@@ -123,15 +52,6 @@ city3.addCitizen();
 console.log(city.getPopulation());
 console.log(city3.getCityName());
 
-
-function ask(question, answer, ok, fail) {
-  var result = prompt(question, '');
-  if (result === answer) {
-    ok();
-  } else {
-    fail();
-  }
-}
 var user = {
   login: 'Andrew',
   password: '12345',
@@ -181,6 +101,85 @@ var secondUser = {
 var user3 = secondUser;
  secondUser = null;
 user3.checkPassword();
+
+function getName() {
+  return this.name;
+}
+
+function getDoubled() {
+  return this.number = this.number * 2;
+}
+
+function getDoubledTrippled(){
+  return getDoubled.call(numObj) * 3;
+}
+
+function Car(brand, year, run, color) {
+  this.brand = brand;
+  this.year = year;
+  this.run = run;
+  this.color = color;
+  this.engine = false;
+  this.traffic = false;
+  this.fuel = false;
+
+  this.startEngine = function() {
+    if(this.fuel === true) {
+      this.engine = true;
+    } else {
+      console.log('Нужно заправить автомобиль');
+    }
+  }
+
+  this.go = function() {
+    if(this.engine === true) {
+      console.log('Машина ' + this.brand + ' марки ' + this.color + ' цвета поехала!');
+      this.traffic = true;
+    } else {
+      console.log('Включите вначале зажигание!');
+    }
+  };
+
+  this.stop = function() {
+    if(this.engine === true) {
+      console.log('Машинка остановилась!');
+      this.engine = false;
+      this.traffic = false;
+    } else {
+      console.log('Зажигание и так выключено');
+    }
+  };
+
+  this.addFuel = function() {
+    this.fuel = true;
+  };
+}
+function City(name, population, country) {
+  this.name = name;
+  this.population = population;
+  this.country = country;
+
+  this.getPopulation = function() {
+    return this.population;
+  };
+
+  this.getCityName = function() {
+    return this.name;
+  };
+
+  this.addCitizen = function() {
+    return this.population++;
+  };
+}
+
+function ask(question, answer, ok, fail) {
+  var result = prompt(question, '');
+  if (result === answer) {
+    ok();
+  } else {
+    fail();
+  }
+}
 
 ;(function() {
   var arr = [
